7e89d20ee6d9136527a4da4ee866c028a7934850
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-27 15:43:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,8 +11,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
 
     if lowest_value == 0 || value < lowest_value
-    lowest_value = value
-    lowest_key = key
+    low_value = value
+    low_key = key
     end
   end
 lowest_key
